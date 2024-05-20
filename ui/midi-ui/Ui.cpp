5579dcdd154f9bd34e4ffa5fe18d0c47b97d597a
Henry Hoegelow <henry@quelltextlabor.de>
2024-05-20 20:01:31 +0200

    add color adjustment

--- ui/midi-ui/Ui.cpp
+++ ui/midi-ui/Ui.cpp
@@ -8,6 +8,79 @@
 namespace Ui::Midi
 {
 
+  struct LedColor
+  {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+
+    static LedColor from(Color c)
+    {
+      switch(c)
+      {
+        case Color::Yellow:
+          return { 255, 255, 0 };
+        case Color::Orange:
+          return { 255, 80, 0 };
+        case Color::Red:
+          return { 255, 0, 0 };
+        case Color::Purple:
+          return { 248, 0, 70 };
+        case Color::Blue:
+          return { 0, 0, 255 };
+        case Color::LightBlue:
+          return { 0, 255, 255 };
+        case Color::LightGreen:
+          return { 0, 255, 120 };
+        case Color::Green:
+          return { 0, 255, 0 };
+        case Color::White:
+          return { 255, 255, 255 };
+        case Color::Off:
+        case Color::None:
+        default:
+          return {};
+      }
+    }
+  };
+
+  struct DisplayColor
+  {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+
+    static DisplayColor from(Color c)
+    {
+      switch(c)
+      {
+        case Color::Yellow:
+          return { 255, 245, 20 };
+        case Color::Orange:
+          return { 255, 152, 0 };
+        case Color::Red:
+          return { 255, 70, 0 };
+        case Color::Purple:
+          return { 248, 94, 210 };
+        case Color::Blue:
+          return { 0, 80, 255 };
+        case Color::LightBlue:
+          return { 20, 215, 255 };
+        case Color::LightGreen:
+          return { 100, 255, 200 };
+        case Color::Green:
+          return { 90, 255, 80 };
+        case Color::White:
+          return { 255, 255, 255 };
+          
+        case Color::Off:
+        case Color::None:
+        default:
+          return {};
+      }
+    }
+  };
+
   Ui::Ui(std::string midiDevice, Core::Api::Interface &core, Dsp::Api::Display::Interface &dsp,
          ::Ui::Touch::Interface &touchUi, ::Ui::Controller &controller)
       : m_midiDevice(std::move(midiDevice))
@@ -44,22 +117,24 @@ namespace Ui::Midi
 
   void Ui::setLed(Knob k, Color c)
   {
-    printf("set led for %s  with color %s\n", getKnobName(k).c_str(), getColorName(c).c_str());
     uint8_t firstLed = static_cast<uint8_t>(Led::FirstCenterKnob);
-    printf("first led for a knob is %d\n", firstLed);
     uint8_t numKnob = static_cast<uint8_t>(k);
-    printf("the order number of the knob is %d\n", numKnob);
     uint8_t numLedsPerKnob = 4;
     uint8_t led = firstLed + numKnob * numLedsPerKnob;
-    printf("so the first led number for the knob is %d\n", led);
 
     for(auto &a : m_inputDevices)
     {
       for(uint8_t b = 0; b < numLedsPerKnob; b++)
       {
         auto l = static_cast<uint8_t>(led + b);
-        printf("sending led number %d\n", l);
-        a.second->send({ 0x94, l, static_cast<uint8_t>(c) });
+        auto ledColor = LedColor::from(c);
+
+        for(auto &a : m_inputDevices)
+        {
+          a.second->send({ 0x91, l, static_cast<uint8_t>(ledColor.r / 2) });
+          a.second->send({ 0x92, l, static_cast<uint8_t>(ledColor.g / 2) });
+          a.second->send({ 0x93, l, static_cast<uint8_t>(ledColor.b / 2) });
+        }
       }
     }
 
@@ -68,24 +143,8 @@ namespace Ui::Midi
 
   void Ui::setLed(SoftButton k, Color c)
   {
-    uint8_t led = 0;
-
-    if((k >= SoftButton::FirstLeftButton && k <= SoftButton::LastLeftButton))
-    {
-      uint8_t offset = static_cast<uint8_t>(k) - static_cast<uint8_t>(SoftButton::FirstLeftButton);
-      led = static_cast<uint8_t>(Led::FirstLeftButton) + offset;
-    }
-
-    if((k >= SoftButton::FirstRightButton && k <= SoftButton::LastRightButton))
-    {
-      uint8_t offset = static_cast<uint8_t>(k) - static_cast<uint8_t>(SoftButton::FirstRightButton);
-      led = static_cast<uint8_t>(Led::FirstRightButton) + offset;
-    }
-
-    for(auto &a : m_inputDevices)
-      a.second->send({ 0x94, led, static_cast<uint8_t>(c) });
-
-    scheduleLedUpdate();
+    auto ledColor = LedColor::from(c);
+    setLed(k, ledColor.r, ledColor.g, ledColor.b);
   }
 
   void Ui::setLed(SoftButton k, uint8_t r, uint8_t g, uint8_t b)
@@ -106,9 +165,9 @@ namespace Ui::Midi
 
     for(auto &a : m_inputDevices)
     {
-      a.second->send({ 0x91, led, static_cast<uint8_t>(r) });
-      a.second->send({ 0x92, led, static_cast<uint8_t>(g) });
-      a.second->send({ 0x93, led, static_cast<uint8_t>(b) });
+      a.second->send({ 0x91, led, static_cast<uint8_t>(r / 2) });
+      a.second->send({ 0x92, led, static_cast<uint8_t>(g / 2) });
+      a.second->send({ 0x93, led, static_cast<uint8_t>(b / 2) });
     }
 
     scheduleLedUpdate();
@@ -116,8 +175,14 @@ namespace Ui::Midi
 
   void Ui::setLed(Step k, Color c)
   {
+    auto ledColor = LedColor::from(c);
+
     for(auto &a : m_inputDevices)
-      a.second->send({ 0x94, k, static_cast<uint8_t>(c) });
+    {
+      a.second->send({ 0x91, k, static_cast<uint8_t>(ledColor.r / 2) });
+      a.second->send({ 0x92, k, static_cast<uint8_t>(ledColor.g / 2) });
+      a.second->send({ 0x93, k, static_cast<uint8_t>(ledColor.b / 2) });
+    }
 
     scheduleLedUpdate();
   }
