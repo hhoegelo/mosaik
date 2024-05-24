0c80d4140ab73558fb4cf74f92d7a556fef589ec
Henry Hoegelow <henry@quelltextlabor.de>
2024-05-24 11:39:08 +0200

    all toolboxes maximized

--- ui/touch-ui/toolboxes/Toolbox.cpp
+++ ui/touch-ui/toolboxes/Toolbox.cpp
@@ -2,11 +2,13 @@
 #include "ui/touch-ui/Interface.h"
 #include <gtkmm/label.h>
 #include <gtkmm/eventbox.h>
+#include <gtkmm/scrolledwindow.h>
+#include <gtkmm/adjustment.h>
 
 namespace Ui::Touch
 {
 
-  Toolbox::Toolbox(ToolboxesInterface &toolboxes, Ui::Toolbox tool, Gtk::Widget *minimzed, Gtk::Widget *maximized)
+  Toolbox::Toolbox(ToolboxesInterface &toolboxes, Ui::Toolbox tool, Gtk::Widget *, Gtk::Widget *maximized)
   {
     set_orientation(Gtk::ORIENTATION_VERTICAL);
 
@@ -15,28 +17,56 @@ namespace Ui::Touch
     set_size_request(540, -1);
 
     auto eventBox = Gtk::manage(new Gtk::EventBox());
-    auto box = Gtk::manage(new Gtk::Box(Gtk::ORIENTATION_VERTICAL));
-    eventBox->add(*box);
+    eventBox->add(*maximized);
 
     eventBox->signal_button_press_event().connect(
-        [=, &toolboxes](GdkEventButton *)
+        [tool, &toolboxes](GdkEventButton *)
         {
           toolboxes.selectToolbox(tool);
           return true;
         });
 
-    box->pack_start(*minimzed, Gtk::PACK_SHRINK);
-    pack_start(*eventBox, Gtk::PACK_SHRINK);
-    pack_start(*maximized, Gtk::PACK_EXPAND_WIDGET);
+    pack_start(*eventBox, Gtk::PACK_EXPAND_WIDGET);
 
-    auto checkChildVisibility = [=, &toolboxes]
-    {
-      auto sel = toolboxes.getSelectedToolbox() == tool;
-      minimzed->set_visible(!sel);
-      maximized->set_visible(sel);
-    };
+    m_computations.add(
+        [this, &toolboxes, tool]
+        {
+          if(toolboxes.getSelectedToolbox() == tool)
+          {
+            get_style_context()->add_class("selected");
 
-    m_computations.add(checkChildVisibility);
-    signal_realize().connect(checkChildVisibility);
+            Gtk::Container *parent = get_parent();
+
+            while(parent && !dynamic_cast<Gtk::ScrolledWindow *>(parent))
+            {
+              parent = parent->get_parent();
+            }
+
+            if(auto w = dynamic_cast<Gtk::ScrolledWindow *>(parent))
+            {
+              auto vadjustment = w->get_vadjustment();
+              auto rect = get_allocation();
+              auto min = vadjustment->get_value();
+              auto max = min + vadjustment->get_page_size();
+
+              if(rect.get_y() < min)
+              {
+                vadjustment->set_value(rect.get_y());
+                w->set_vadjustment(vadjustment);
+              }
+
+              if(rect.get_y() + rect.get_height() > max)
+              {
+                auto spaceAbove = (vadjustment->get_page_size() - rect.get_height());
+                vadjustment->set_value(rect.get_y() - spaceAbove);
+                w->set_vadjustment(vadjustment);
+              }
+            }
+          }
+          else
+          {
+            get_style_context()->remove_class("selected");
+          }
+        });
   }
 }
